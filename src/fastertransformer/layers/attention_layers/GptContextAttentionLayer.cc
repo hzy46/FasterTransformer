18785826149c2022527b638fba287f5306a75a31
hzy46 <362583303@qq.com>
2023-04-14 14:17:11 +0800

    fix

--- src/fastertransformer/layers/attention_layers/GptContextAttentionLayer.cc
+++ src/fastertransformer/layers/attention_layers/GptContextAttentionLayer.cc
@@ -47,9 +47,9 @@ void GptContextAttentionLayer<T>::forward(TensorMap*                output_tenso
     FT_CHECK(output_tensors->at("key_cache").shape.size() == 5);
     FT_CHECK(output_tensors->at("value_cache").shape.size() == 4
              || output_tensors->at("value_cache").shape.size() == 3);
-    print('[GptContextAttentionLayer->forward] key_cache:');
+    printf("[GptContextAttentionLayer->forward] key_cache:");
     output_tensors->at("key_cache").print_shape();
-    print('[GptContextAttentionLayer->forward] value_cache:');
+    printf("[GptContextAttentionLayer->forward] value_cache:");
     output_tensors->at("value_cache").print_shape();
     const int request_batch_size = input_tensors->at("attention_mask").shape[0];
     const int request_seq_len    = input_tensors->at("attention_mask").shape[2];
